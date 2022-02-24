7e83d2ad28e9e623b4cd0a9a4f6205d669cb8bcc
Toshichika Mashimo <mashita1023@gmail.com>
2022-02-25 03:04:43 +0900

    add handsontable

---
+++ src/pages/handsontable/index.tsx
@@ -0,0 +1,21 @@
+import "react";
+import { HotTable } from '@handsontable/react'
+import 'handsontable/dist/handsontable.full.css'
+
+
+export const HandsontablePage = () => {
+  const data = [
+    ['', 'Tesla', 'Mercedes', 'toyota', 'Volvo'],
+    ['2019', 10, 11, 12, 13],
+    ['2020', 20, 11, 14, 13]
+  ]
+  return (
+    <HotTable
+      data={data}
+      colHeaders={true}
+      rowHeaders={true}
+      width='600'
+      height='300'
+    />
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,34 @@
 import { Routes, Route } from 'react-router-dom'
 import { TodoPage } from '../pages/todo'
 import { NotFound } from '../pages/error/notfound'
+import { HandsontablePage } from '../pages/handsontable'
 
 export const AppRoute = () => {
   return (
     <Routes>
       {/* 404 */}
-      <Route path='*' element={<NotFound />} />
+      <Route
+        path='*'
+        element={<NotFound />}
+      />
 
       {/* root */}
-      <Route index element={<TodoPage />} />
+      <Route
+        index
+        element={<TodoPage />}
+      />
 
       {/* todo */}
-      <Route path='todo' element={<TodoPage />} />
+      <Route
+        path='todo'
+        element={<TodoPage />}
+      />
+
+      {/* handsontable */}
+      <Route
+        path='handsontable'
+        element={<HandsontablePage />}
+      />
 
     </Routes>
   )
