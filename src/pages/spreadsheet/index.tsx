137687d1f31618bfb675adb79a3bfe5597b5f75a
Toshichika Mashimo <mashita1023@gmail.com>
2022-02-28 08:06:42 +0900

    end test

--- src/pages/spreadsheet/index.tsx
+++ src/pages/spreadsheet/index.tsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
-import { Spreadsheet } from 'react-spreadsheet'
+import { CellBase, createEmptyMatrix, Spreadsheet } from 'react-spreadsheet'
 
 export const SpreadsheetPage = () => {
+  /*
   const [data, setData] = useState([
     [{ value: 'Vanilla' }, { value: 'Chocolate' }],
     [{ value: 'Strawberry' }, { value: 'Cookies' }],
   ])
+*/
+  const [data, setData] = useState(createEmptyMatrix<CellBase>(5, 5))
 
-  return <Spreadsheet data={data} onChange={() => setData} />
+  const columnLabels = ['test', 'test#2', 'test#3', 'test#4']
+
+  const rowLabels = ['名前', '局', '6:00', '7:00', '8:00', '9:00', '10:00']
+
+  return (
+    <Spreadsheet
+      data={data}
+      columnLabels={columnLabels}
+      rowLabels={rowLabels}
+      onChange={(e) => {
+        console.log(e)
+        console.log(createEmptyMatrix)
+        setData
+      }}
+    />
+  )
 }
