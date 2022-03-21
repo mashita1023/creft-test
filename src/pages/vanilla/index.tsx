705aaa1d68da527ab37fae0726073563960a3d65
Toshichika Mashimo <mashita1023@gmail.com>
2022-03-22 06:02:08 +0900

    fix test excel page

--- src/pages/vanilla/index.tsx
+++ src/pages/vanilla/index.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import json from '../../assets/api.json'
 import './index.css'
 type Json = typeof json
@@ -10,25 +12,140 @@ type Task = {
 }
 
 type User = {
+  id: number
   name: string
   bureau: string
   tasks: Task[]
 }
 
+const newUser = () => {
+  const user: User[] = [
+    {
+      id: 0,
+      name: '',
+      bureau: '',
+      tasks: [
+        {
+          task_id: 0,
+          task: '',
+          time_id: 0,
+          time: '',
+        },
+      ],
+    },
+  ]
+
+  return user
+}
+
+const addUser = (user: User[]) => {
+  user.push({
+    id: user[-1].id + 1,
+    name: '',
+    bureau: '',
+    tasks: [
+      {
+        task_id: 0,
+        task: '',
+        time_id: 0,
+        time: '',
+      },
+    ],
+  })
+
+  return user
+}
+
 const user: Json = json
 export const VanillaPage = () => {
-  const renderTask = (u: User) => {
-    return u.tasks.map((t, i) => {
-      return <td key={u.name + 'task-' + i}>{t.task}</td>
+  const [shifts, setShifts] = useState<User[]>(newUser)
+
+  let unmounted = false
+
+  const handleOnEditName = (userId: number, name: string) => {
+    const deepCopy = shifts.map((shift) => ({ ...shift }))
+
+    const newShifts = deepCopy.map((shift) => {
+      if (shift.id === userId) {
+        shift.name = name
+      }
+      return shift
     })
+
+    setShifts(newShifts)
   }
+
+  const handleOnEditBureau = (userId: number, bureau: string) => {
+    const deepCopy = shifts.map((shift) => ({ ...shift }))
+
+    const newShifts = deepCopy.map((shift) => {
+      if (shift.id === userId) {
+        shift.bureau = bureau
+      }
+      return shift
+    })
+
+    setShifts(newShifts)
+  }
+
+  const handleOnEditTask = (userId: number, timeId: number, task: string) => {
+    const deepCopy = shifts.map((shift) => ({ ...shift }))
+
+    const newShifts = deepCopy.map((shift) => {
+      if (shift.id === userId) {
+        shift.tasks.map((t) => {
+          if (t.time_id === timeId) {
+            t.task = task
+          }
+          return t
+        })
+      }
+      return shift
+    })
+
+    setShifts(newShifts)
+  }
+
+  useEffect(() => {
+    async function getJson() {
+      setShifts(json.users)
+    }
+
+    getJson()
+    unmounted = true
+  }, [unmounted])
+
   const renderRows = () => {
-    return user.users.map((u, i) => {
+    return shifts.map((u, i) => {
       return (
         <tr key={'item-' + i}>
-          <th key={'user-' + i}>{u.name}</th>
-          <th key={'bureau-' + i}>{u.bureau}</th>
-          {renderTask(u)}
+          <th key={'user-' + i}>
+            <input
+              type="text"
+              value={u.name}
+              onChange={(e) => handleOnEditName(u.id, e.target.value)}
+            />
+          </th>
+          <th key={'bureau-' + i}>
+            <input
+              type="text"
+              value={u.bureau}
+              onChange={(e) => handleOnEditBureau(u.id, e.target.value)}
+            />
+          </th>
+          {u.tasks.map((t, j) => {
+            return (
+              <td key={u.name + 'task-' + j}>
+                <input
+                  type="text"
+                  value={t.task}
+                  onChange={(e) =>
+                    handleOnEditTask(u.id, t.time_id, e.target.value)
+                  }
+                />
+              </td>
+            )
+          })}
         </tr>
       )
     })
